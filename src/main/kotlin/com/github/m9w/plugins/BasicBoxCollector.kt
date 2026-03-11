e463b0f0487282e324ce4612a4f7d2b656e279e5
m9w <zc.besaba@gmail.com>
2026-03-11 19:35:29 +0200

    BoxCollector upgrade

--- src/main/kotlin/com/github/m9w/plugins/BasicBoxCollector.kt
+++ src/main/kotlin/com/github/m9w/plugins/BasicBoxCollector.kt
@@ -3,48 +3,77 @@ package com.github.m9w.plugins
 import com.darkorbit.*
 import com.github.m9w.client.GameEngine
 import com.github.m9w.collectRequest
+import com.github.m9w.config.config
 import com.github.m9w.context.context
 import com.github.m9w.feature.annotations.OnPackage
 import com.github.m9w.feature.annotations.Repeat
 import com.github.m9w.metaplugins.EntitiesModule
+import com.github.m9w.metaplugins.game.Point
 import com.github.m9w.metaplugins.game.PositionImpl.Companion.distanceTo
-import com.github.m9w.metaplugins.game.PositionImpl.Companion.x
-import com.github.m9w.metaplugins.game.PositionImpl.Companion.y
 import com.github.m9w.metaplugins.game.entities.BoxImpl
 import com.github.m9w.metaplugins.game.entities.BoxImpl.Companion.name
+import com.github.m9w.metaplugins.game.entities.HeroShip
 import io.ktor.util.date.getTimeMillis
+import java.util.concurrent.ConcurrentHashMap
 
 @Suppress("unused")
-open class BasicBoxCollector {
+class BasicBoxCollector {
+    val targetNames by config(mutableSetOf<String>())
+
     private val entities: EntitiesModule by context
-    protected val targetNames = mutableSetOf("BONUS_BOX", "SOLAR_CLASH")
+    private val engine: GameEngine by context
     private val targets: MutableMap<String, AddBoxCommand> = HashMap()
     private var best: AddBoxCommand? = null
     private var lastCollect = 0L
+    private var timeout = 0L
+    private var place: Point = -100 to -100
 
     @OnPackage
     private fun onHeroInit(init: ShipInitializationCommand) { targets.clear(); best = null }
 
     @OnPackage
-    private fun boxCreate(event: AddBoxCommand) { if (targetNames.contains(event.name)) targets.put(event.hash, event).also { calcBest() } }
-
-    @OnPackage
-    private fun boxRemove(event: RemoveCollectableCommand) { if (event.collected) removeHash(event.hash).also { calcBest() } }
-
-    @Repeat(100)
-    private fun calcBest() {
-        if (entities.gameEngine.state != GameEngine.State.NORMAL) return
-        best = targets.values.minByOrNull { entities.hero.position.distanceTo(it.x to it.y) }
-        if (best == null && !entities.hero.isMoving) entities.hero.moveRandom()
-        val best = best ?: return
-        if (entities.hero.destination.position.run { x == best.x && y == best.y } ) {
-            if (lastCollect + 1500 < getTimeMillis()) collect(best)
-        } else entities.hero.moveTo(best.x to best.y) { collect(best) }
+    private fun boxCreate(event: AddBoxCommand) {
+        if (!bugged.contains(event.hash) && targetNames.contains(event.name)) {
+            targets[event.hash] = event
+            calcBest()
+        }
     }
 
+    @OnPackage
+    private fun boxRemove(event: RemoveCollectableCommand) { if (event.collected) removeHash(event.hash) }
+
+    @Repeat(1000)
+    private fun calcBest() {
+        if (engine.state != GameEngine.State.NORMAL) return
+        val h = best?.hash
+        best = targets.values.minByOrNull { entities.hero.position.distanceTo(it.point) }
+        if (best?.hash != h) lastCollect = 0
+        if (best == null && !entities.hero.isMoving) entities.hero.continueMove()
+        val best = best ?: return
+        if (entities.hero.position.distanceTo(best.point) < 400) {
+            if (place != best.point) {
+                timeout = getTimeMillis()
+                place = best.point
+            } else if (timeout + 30*1000 < getTimeMillis()) {
+                timeout = getTimeMillis()
+                place = -100 to -100
+                bug(best.hash)
+                removeHash(best.hash)
+            }
+
+            if (lastCollect + 750 < getTimeMillis()) {
+                collect(best)
+                if (!entities.hero.isMoving)
+                    entities.hero.moveTo(best.point) { collect(best) }
+            }
+        } else entities.hero.moveTo(best.point) { collect(best) }
+    }
+
+    @Repeat(333000) fun bugCleanup() = cleanup()
+
     private fun collect(box: AddBoxCommand) {
-        entities[box.hash.toLong(36)+Int.MAX_VALUE]?.let {
-            entities.gameEngine.collectRequest(entities.hero, it as BoxImpl)
+        entities[box.hash.toLong(36)+Int.MAX_VALUE]?.also {
+            engine.collectRequest(entities.hero, it as BoxImpl)
             lastCollect = getTimeMillis()
             calcBest()
         } ?: removeHash(box.hash)
@@ -52,6 +81,22 @@ open class BasicBoxCollector {
 
     private fun removeHash(hash: String) {
         targets.remove(hash)
-        if (best?.hash == hash) best = null
+        val moving = entities.hero.isMoving
+        calcBest()
+        if (moving && best == null) entities.hero.continueMove()
+    }
+
+    private fun HeroShip.continueMove() = moveRandom { calcBest() }
+
+    companion object {
+        private val AddBoxCommand.point: Point get() = x to y
+        private val bugged = ConcurrentHashMap<String, Long>()
+        private fun bug(hash: String) = bugged.put(hash, getTimeMillis())
+        private var lastCheck = 0L
+        private fun cleanup() {
+            if (lastCheck + 5*60*1000 > getTimeMillis()) return
+            bugged.entries.removeIf { it.value + 30*60*1000 < getTimeMillis() }
+            lastCheck = getTimeMillis()
+        }
     }
 }

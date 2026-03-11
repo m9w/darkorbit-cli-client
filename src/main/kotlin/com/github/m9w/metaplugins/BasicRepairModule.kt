c67f30f29601bdaead384c7a73e7eaba5e2dc977
m9w <zc.besaba@gmail.com>
2026-03-11 19:39:04 +0200

    Add auto repair feature

--- src/main/kotlin/com/github/m9w/metaplugins/BasicRepairModule.kt
+++ src/main/kotlin/com/github/m9w/metaplugins/BasicRepairModule.kt
@@ -4,19 +4,26 @@ import com.darkorbit.KillScreenPostCommand
 import com.darkorbit.KillScreenRepairRequest
 import com.github.m9w.client.GameEngine
 import com.github.m9w.client.auth.AuthenticationProvider
+import com.github.m9w.config.config
 import com.github.m9w.context.context
 import com.github.m9w.feature.annotations.OnPackage
+import com.github.m9w.feature.annotations.Repeat
 import com.github.m9w.feature.waitMs
+import com.github.m9w.metaplugins.game.entities.JumpgateImpl
+import kotlin.collections.filter
 
 class BasicRepairModule {
-    private val gameEngine: GameEngine by context
+    private val entities: EntitiesModule by context
+    private val engine: GameEngine by context
+    private val map: MapModule by context
     private val authentication: AuthenticationProvider by context
+    private val hpPercentalForRepairing: Double by config(0.20)
+    private val enabled: Boolean by config(true)
 
     @OnPackage
     private suspend fun mapUpdate(screen: KillScreenPostCommand) {
         waitMs(1000)
-        // if(gameEngine.state != GameEngine.State.DESTROYED) return
-        gameEngine.send<KillScreenRepairRequest> {
+        engine.send<KillScreenRepairRequest> {
             selection = screen.options[0].repairType
             requestModule = requestModule.apply {
                 userID = authentication.userID
@@ -25,5 +32,26 @@ class BasicRepairModule {
                 isMiniClient = false
             }
         }
+    }
+
+    @Repeat(1000)
+    private fun autoRepair() {
+        if (!enabled) return
+
+        if ((engine.state == GameEngine.State.NORMAL || engine.state == GameEngine.State.TRAVELING)
+            && entities.hero.health.health < entities.hero.health.healthMax*hpPercentalForRepairing)
+            engine.state = GameEngine.State.REPAIRING
+
+        if (engine.state == GameEngine.State.REPAIRING && !map.inPeaceArea)
+            entities.get<JumpgateImpl>()
+                .filter { it.designId == 1 }
+                .minBy { it.distanceTo(entities.hero) }
+                .takeIf { entities.hero.destination.distanceTo(it) > 100 }
+                ?.let { entities.hero.moveTo(it.position ) }
+
+        if (engine.state == GameEngine.State.REPAIRING
+            && entities.hero.health.health > entities.hero.health.healthMax-100
+            && entities.hero.health.shield > entities.hero.health.shieldMax-100)
+            engine.state = GameEngine.State.NORMAL
     }
 }
